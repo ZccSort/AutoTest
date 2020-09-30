a666ed2b3931ae127822d899864e8bcd3797c8d5
zhouchao <1762156671@qq.coom>
2020-09-30 23:16:26 +0800

    TestNG+MyBatis实现数据校验

---
+++ Chapter13/src/main/java/com/course/model/User.java
@@ -0,0 +1,37 @@
+package com.course.model;
+
+import lombok.Data;
+
+/**
+ * 用户信息
+ */
+@Data
+public class User {
+
+    private  int id;
+
+    private  String userName;
+
+    private  String password;
+    private String  age;
+
+    private String sex;
+
+    private String permission;
+    //是否删除
+    private String isDelete;
+
+    /*@Override
+    public String toString(){
+         return (
+                 "{id:"+id+","+
+                 "userName:"+userName+","+
+                 "password:"+password+","+
+                 "age:"+age+","+
+                 "sex:"+sex+","+
+                 "permission:"+permission+","+
+                 "isDelete:"+isDelete+"}"
+                 );
+    }*/
+
+}
